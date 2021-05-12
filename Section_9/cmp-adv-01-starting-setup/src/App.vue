0bb52c5059ef7f761e2478274a6c74d082c464b2
pkysylev <pavlo.kysylevych@gmail.com>
2021-05-12 23:27:46 +0300

    Diving Into Components

---
+++ Section_9/cmp-adv-01-starting-setup/src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
+
+    <!-- <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+    <course-goals #default="slotProps">
+      <h2>{{ slotProps.item }}</h2>
+      <p>{{ slotProps.anotherProp }}</p>
+    </course-goals> -->
+
+    <!-- <course-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps.anotherProp }}</p>
+      </template>
+    </course-goals> -->
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/layout/TheHeader.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import CourseGoals from "./components/CourseGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
+
+export default {
+  components: {
+    TheHeader,
+    ActiveGoals,
+    ManageGoals,
+    // UserInfo,
+    // BadgeList,
+    // CourseGoals,
+    // 'the-header': TheHeader,
+    // 'user-info': UserInfo
+    // 'badge-list': BadgeList
+  },
+  data() {
+    return {
+      selectedComponent: "active-goals",
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
