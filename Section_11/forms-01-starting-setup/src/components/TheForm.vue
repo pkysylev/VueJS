2175896ba23596d954d1119773d8739064c4acf0
pkysylev <pavlo.kysylevych@gmail.com>
2021-05-14 00:55:22 +0300

    Integrated RatingControl component into TheForm.vue

--- Section_11/forms-01-starting-setup/src/components/TheForm.vue
+++ Section_11/forms-01-starting-setup/src/components/TheForm.vue
@@ -94,6 +94,9 @@
       </div>      
     </div>
     <div class="form-control">
+      <rating-control></rating-control>      
+    </div>
+    <div class="form-control">
         <input
           type="checkbox"
           id="confirm-terms"
@@ -109,7 +112,12 @@
 </template>
 
 <script>
+import RatingControl from "./RatingControl.vue"
+
 export default {
+  components: {
+    RatingControl
+  },
   data() {
     return {
       userName: '',
