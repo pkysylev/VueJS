5b900ee819d458c13eb7577597fe8cca4f931fc9
pkysylev <pavlo.kysylevych@gmail.com>
2021-05-14 00:52:12 +0300

    Building a Custom Control Component

---
+++ Section_11/forms-01-starting-setup/src/components/RatingControl.vue
@@ -0,0 +1,61 @@
+<template>
+  <ul>
+    <li :class="{ active: activeOptions === 'poor' }">
+      <button type="button" @click="activate('poor')">Poor</button>
+    </li>
+    <li :class="{ active: activeOptions === 'average' }">
+      <button type="button" @click="activate('average')">Average</button>
+    </li>
+    <li :class="{ active: activeOptions === 'great' }">
+      <button type="button" @click="activate('great')">Great</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeOptions: null,
+    };
+  },
+  methods: {
+    activate(option) {
+      this.activeOptions = option;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  border-color: #a00078;
+}
+
+.active button {
+  border-color: #a00078;
+}
+
+ul {
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  font: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
