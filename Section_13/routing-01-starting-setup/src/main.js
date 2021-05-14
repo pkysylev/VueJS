3d79a0e195eed900bdfba9c57aaa2115de0a9e8f
pkysylev <pavlo.kysylevych@gmail.com>
2021-05-15 01:57:48 +0300

    Redirecting & "Catch All" Routers

--- Section_13/routing-01-starting-setup/src/main.js
+++ Section_13/routing-01-starting-setup/src/main.js
@@ -5,13 +5,18 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue'
 import UsersList from './components/users/UsersList.vue'
 import TeamMembers from './components/teams/TeamMembers.vue'
+import NotFound from './components/nav/NotFound.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        { path: '/', redirect: '/teams' },
         { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
+        // { path: '/teams', component: TeamsList, alias: '/' }, // our-domain.com/teams => TeamsList
         { path: '/users', component: UsersList }, // our-domain.com/users => UsersList
-        { path: '/teams/:teamId', component: TeamMembers, props: true }
+        { path: '/teams/:teamId', component: TeamMembers, props: true },
+        // { path: '/:notFound(.*)', redirect: '/teams' }
+        { path: '/:notFound(.*)', component: NotFound }
     ],
     linkActiveClass: 'active'
 });
