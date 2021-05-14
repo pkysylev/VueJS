02051b78dca1c069bf24fd647623dfbce41292fe
pkysylev <pavlo.kysylevych@gmail.com>
2021-05-14 21:04:40 +0300

    Passing Data with Route Params (Dynamic Segments)

--- Section_13/routing-01-starting-setup/src/main.js
+++ Section_13/routing-01-starting-setup/src/main.js
@@ -4,12 +4,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue'
 import UsersList from './components/users/UsersList.vue'
+import TeamMembers from './components/teams/TeamMembers.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
         { path: '/users', component: UsersList }, // our-domain.com/users => UsersList
+        { path: '/teams/:teamId', component: TeamMembers}
     ],
     linkActiveClass: 'active'
 });
