9aaf4939a7a70fad6fe0b32a9d717bbd4e21f108
pkysylev <pavlo.kysylevych@gmail.com>
2021-05-15 21:14:56 +0300

    Whe Is "Just CSS" Not Enough?

--- Section_14/animations-01-starting-setup/src/components/BaseModal.vue
+++ Section_14/animations-01-starting-setup/src/components/BaseModal.vue
@@ -34,5 +34,17 @@ dialog {
   background-color: white;
   z-index: 100;
   border: none;
+  animation: modal 0.3s ease-out forwards;
+}
+
+@keyframes modal {
+  from {
+    opacity: 0;
+    transform: translateY(-50%) scale(0.9);
+  }
+
+  to {
+    transform: translateY(0) scale(1);
+  }
 }
 </style>
