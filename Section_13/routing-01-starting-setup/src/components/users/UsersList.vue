bbc7212c4dd68ef154056ed31271763c2a92d88f
pkysylev <pavlo.kysylevych@gmail.com>
2021-05-15 03:29:23 +0300

    Beyon Entering: Route Leave Guards

--- Section_13/routing-01-starting-setup/src/components/users/UsersList.vue
+++ Section_13/routing-01-starting-setup/src/components/users/UsersList.vue
@@ -1,7 +1,13 @@
 <template>
-<button @click="confirmInput">Confirm</button>
+  <button @click="confirmInput">Confirm</button>
+  <button @click="saveChanges">Save Changes</button>
   <ul>
-    <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
+    <user-item
+      v-for="user in users"
+      :key="user.id"
+      :name="user.fullName"
+      :role="user.role"
+    ></user-item>
   </ul>
 </template>
 
@@ -13,12 +19,35 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  data() {
+    return {
+      changesSaved: false,
+    };
+  },
   methods: {
-    confirmInput(){
+    confirmInput() {
       // do something
       this.$router.push('/teams');
       //this.$router.forward() - read an official documentation
+    },
+    saveChanges() {
+      this.changesSaved = true;
+    },
+  },
+  beforeRouteLeave(to, from, next) {
+    console.log('UsersList Component beforeRouteLeave');
+    console.log(to, from);
+    if (this.changesSaved) {
+      next();
+    } else {
+      const userWantsToLeave = confirm('Are you sure? You got unsaved changes! \r\n You have to click Save Changes first!');
+      next(userWantsToLeave);
     }
+  },
+  unmounted() {
+    // Problem is that this runs after the hooks, after the navigation has been confirmed
+    // and this gives us no way of canceling the navigation
+    console.log('unmounted');
   },
   // beforeRouteEnter(to, from, next){
   //   console.log('UsersList Component beforeRouteEnter');
