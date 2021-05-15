e9080b3bd833be66577b41d2819b819c1d7872de
pkysylev <pavlo.kysylevych@gmail.com>
2021-05-15 20:58:07 +0300

    Animation Basics & CSS Transitions

--- Section_14/animations-01-starting-setup/src/App.vue
+++ Section_14/animations-01-starting-setup/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
-    <div class="block"></div>
-    <button>Animate</button>
+    <div class="block" :class="{ animate: animatedBlock }"></div>
+    <button @click="aninateBlock">Animate</button>
   </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
@@ -15,9 +15,15 @@
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false };
+    return {
+      animatedBlock: false,
+      dialogIsVisible: false,
+    };
   },
   methods: {
+    aninateBlock() {
+      this.animatedBlock = !this.animatedBlock;
+    },
     showDialog() {
       this.dialogIsVisible = true;
     },
@@ -57,6 +63,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
+  transition: transform 0.5s ease-out;
 }
 .container {
   max-width: 40rem;
@@ -68,5 +75,9 @@ button:active {
   padding: 2rem;
   border: 2px solid #ccc;
   border-radius: 12px;
+}
+
+.animate{
+  transform: translateX(-150px);
 }
 </style>
