1375a93b802502c25aba60be4153a695786bb2b4
pkysylev <pavlo.kysylevych@gmail.com>
2021-05-17 00:13:50 +0300

    Introducing Mutations - A Better Way of Changing Data

--- Section_15/vuex-01-starting-setup/src/App.vue
+++ Section_15/vuex-01-starting-setup/src/App.vue
@@ -2,26 +2,24 @@
   <base-container title="Vuex">    
     <the-counter></the-counter>
     <button @click="addOne">Add 1</button>
+    <change-counter></change-counter>
   </base-container>  
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
 import TheCounter from './components/TheCounter'
+import ChangeCounter from './components/ChangeCounter.vue'
 
 export default {
   components: {
     BaseContainer,
-    TheCounter
-  },
-  computed: {
-    counter(){
-      return this.$store.state.counter;
-    }
-  },
+    TheCounter,
+    ChangeCounter
+  },  
   methods: {
     addOne(){
-      this.$store.state.counter++;
+      this.$store.commit('increment');
     }
   }
 };
