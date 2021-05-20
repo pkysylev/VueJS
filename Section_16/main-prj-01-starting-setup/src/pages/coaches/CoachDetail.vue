3ccf94a971c75691b4f37da487841eab6389633f
pkysylev <pavlo.kysylevych@gmail.com>
2021-05-21 01:23:30 +0300

    Working on the Coaches List and List Items

--- Section_16/main-prj-01-starting-setup/src/pages/coaches/CoachDetail.vue
+++ Section_16/main-prj-01-starting-setup/src/pages/coaches/CoachDetail.vue
@@ -3,3 +3,30 @@
     <router-view></router-view>
     <router-link to="/coaches/c1/contact">Contact</router-link>
 </template>
+
+<style scoped>
+li {
+  margin: 1rem 0;
+  border: 1px solid #424242;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+h3 {
+  font-size: 1.5rem;
+}
+
+h3,
+h4 {
+  margin: 0.5rem 0;
+}
+
+div {
+  margin: 0.5rem 0;
+}
+
+.actions {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- Section_16/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ Section_16/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -6,9 +6,14 @@
       <router-link to="/register">Register as Coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in coaches" :key="coach.id">
-          {{ coach.firstName }}
-      </li>
+      <coach-item v-for="coach in coaches" 
+      :key="coach.id"
+      :id="coach.id"
+      :firstName="coach.firstName"
+      :lastName="coach.lastName"
+      :rate="coach.hourlyRate"
+      :areas="coach.areas"
+      ></coach-item>
     </ul>
     <h3 v-else>No coaches found</h3>
   </section>
@@ -16,11 +21,28 @@
 
 <script>
 import { mapGetters } from 'vuex';
+import CoachItem from '../../components/coaches/CoachItem.vue'
 
 export default {
+    components: {
+        CoachItem
+    },
     computed: {
         ...mapGetters('coaches', ['coaches']),
         ...mapGetters('coaches', ['hasCoaches'])
     }
 }
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
