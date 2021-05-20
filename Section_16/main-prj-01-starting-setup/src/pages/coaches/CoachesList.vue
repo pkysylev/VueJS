96fa7bdb8d81029488148e9e8c25fc02bc6c34e5
pkysylev <pavlo.kysylevych@gmail.com>
2021-05-21 01:02:34 +0300

    Adding Vuex and Coach Data

--- Section_16/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ Section_16/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -1,20 +1,26 @@
 <template>
-    <section>
-        FILTER
-    </section>
-    <section>
-        <div class="controls">
-            <button>Refresh</button>
-            <router-link to="/register">Register as Coach</router-link>
-        </div>
-        <ul>LIST OF COACHES</ul>
-    </section>
+  <section>FILTER</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
+    <ul v-if="hasCoaches">
+      <li v-for="coach in coaches" :key="coach.id">
+          {{ coach.firstName }}
+      </li>
+    </ul>
+    <h3 v-else>No coaches found</h3>
+  </section>
 </template>
 
 <script>
-export default {
-    methods: {
+import { mapGetters } from 'vuex';
 
+export default {
+    computed: {
+        ...mapGetters('coaches', ['coaches']),
+        ...mapGetters('coaches', ['hasCoaches'])
     }
 }
 </script>
--- Section_16/main-prj-01-starting-setup/src/store/index.js
+++ Section_16/main-prj-01-starting-setup/src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from 'vuex'
 
+import coachesModule from './modules/coaches/index.js'
+
 const store = createStore({
     modules: {
-        
+        coaches: coachesModule
     },
     state() {
 
