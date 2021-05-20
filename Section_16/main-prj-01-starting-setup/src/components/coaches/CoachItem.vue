8c217a3e2562b228c3ced74fe24876dea79404d2
pkysylev <pavlo.kysylevych@gmail.com>
2021-05-21 01:54:13 +0300

    Adding a Couple of Base Components (Base Card, Base Buttons, Base Badge)

--- Section_16/main-prj-01-starting-setup/src/components/coaches/CoachItem.vue
+++ Section_16/main-prj-01-starting-setup/src/components/coaches/CoachItem.vue
@@ -1,32 +1,41 @@
 <template>
-    <li>
-        <h3>{{ fullName }}</h3>
-        <h4>${{ rate }}/hour</h4>
-        <div>
-            <span v-for="area in areas" :key="area">{{ area }}</span>
-        </div>
-        <div class="actions">
-            <router-link :to="coachContactLink">Contact</router-link>
-            <router-link :to="coachDetailsLink">View Details</router-link>
-        </div>
-    </li>
+  <li>
+    <h3>{{ fullName }}</h3>
+    <h4>${{ rate }}/hour</h4>
+    <div>
+      <base-badge
+        v-for="area in areas"
+        :key="area"
+        :type="area"
+        :title="area"
+      ></base-badge>
+    </div>
+    <div class="actions">
+      <base-button mode="outline" link :to="coachContactLink"
+        >Contact</base-button
+      >
+      <base-button link :to="coachDetailsLink">View Details</base-button>
+    </div>
+  </li>
 </template>
 
 <script>
+import BaseButton from '../ui/BaseButton.vue';
 export default {
-    props: ['id', 'firstName', 'lastName', 'rate', 'areas'],
-    computed: {
-        fullName(){
-            return `${this.firstName} ${this.lastName}`;
-        },
-        coachContactLink(){
-            return `${this.$route.path}/${this.id}/contact`;
-        },
-        coachDetailsLink(){
-            return `${this.$route.path}/${this.id}`;
-        }
-    }
-}
+  components: { BaseButton },
+  props: ['id', 'firstName', 'lastName', 'rate', 'areas'],
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+    coachContactLink() {
+      return `${this.$route.path}/${this.id}/contact`;
+    },
+    coachDetailsLink() {
+      return `${this.$route.path}/${this.id}`;
+    },
+  },
+};
 </script>
 
 <style>
--- Section_16/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ Section_16/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -1,37 +1,46 @@
 <template>
   <section>FILTER</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <coach-item v-for="coach in coaches" 
-      :key="coach.id"
-      :id="coach.id"
-      :firstName="coach.firstName"
-      :lastName="coach.lastName"
-      :rate="coach.hourlyRate"
-      :areas="coach.areas"
-      ></coach-item>
-    </ul>
-    <h3 v-else>No coaches found</h3>
+    <base-card>
+      <div class="controls">
+        <!-- <button>Refresh</button> -->
+        <!-- <router-link to="/register">Register as Coach</router-link> -->
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register"
+          >Register as Coach</base-button
+        >
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in coaches"
+          :key="coach.id"
+          :id="coach.id"
+          :firstName="coach.firstName"
+          :lastName="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches found</h3>
+    </base-card>
   </section>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
-import CoachItem from '../../components/coaches/CoachItem.vue'
+import CoachItem from '../../components/coaches/CoachItem.vue';
+import BaseButton from '../../components/ui/BaseButton.vue';
 
 export default {
-    components: {
-        CoachItem
-    },
-    computed: {
-        ...mapGetters('coaches', ['coaches']),
-        ...mapGetters('coaches', ['hasCoaches'])
-    }
-}
+  components: {
+    CoachItem,
+    BaseButton,
+  },
+  computed: {
+    ...mapGetters('coaches', ['coaches']),
+    ...mapGetters('coaches', ['hasCoaches']),
+  },
+};
 </script>
 
 <style scoped>
