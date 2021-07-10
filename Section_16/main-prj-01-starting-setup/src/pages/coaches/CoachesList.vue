123f1469fd22b87aa8af239b02d7e0fc2f4cd56e
pkysylev <pavlo.kysylevych@gmail.com>
2021-07-10 13:57:14 +0300

    Caching Http Response Data

--- Section_16/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ Section_16/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -8,7 +8,7 @@
   <section>
     <base-card>
       <div class="controls">
-        <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
+        <base-button mode="outline" @click="loadCoaches(true)">Refresh</base-button>
         <base-button link to="/register" v-if="!isCoach && !isLoading"
           >Register as Coach</base-button
         >
@@ -86,10 +86,10 @@ export default {
     setFilter(updatedFilters) {
       this.activeFilters = updatedFilters;
     },
-    async loadCoaches() {
+    async loadCoaches(refresh = false) {
       this.isLoading = true;
       try {
-        await this.$store.dispatch('coaches/loadCoaches');
+        await this.$store.dispatch('coaches/loadCoaches', {forceRefresh: refresh});
       } catch (error) {
         this.error = error.message || 'Something went wrong!';
       }
--- Section_16/main-prj-01-starting-setup/src/store/modules/coaches/actions.js
+++ Section_16/main-prj-01-starting-setup/src/store/modules/coaches/actions.js
@@ -25,7 +25,11 @@ export default {
             id: userId
         });
     },
-    async loadCoaches(context) {
+    async loadCoaches(context, payload) {
+        if(!payload.forceRefresh && !context.getters.shouldUpdate){
+            return;
+        }
+
         const response = await fetch(
             `https://find-a-coach-fe46d-default-rtdb.firebaseio.com/coaches.json`
         );
@@ -51,5 +55,6 @@ export default {
         }
 
         context.commit('setCoaches', coaches);
+        context.commit('setFetchTimestamp');
     }
 };
