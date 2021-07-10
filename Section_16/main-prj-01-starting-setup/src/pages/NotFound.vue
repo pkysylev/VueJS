bf601463f80eb08ca2e6bfdfb392c5409558800e
pkysylev <pavlo.kysylevych@gmail.com>
2021-07-10 14:21:54 +0300

    Added NotFound page

--- Section_16/main-prj-01-starting-setup/src/pages/NotFound.vue
+++ Section_16/main-prj-01-starting-setup/src/pages/NotFound.vue
@@ -1,6 +1,11 @@
 <template>
-  <h2>
-    Page not found ! Maybe view our
-    <router-link to="/coaches">Teams</router-link>?
-  </h2>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found - maybe check out all our
+        <router-link to="/coaches">coaches</router-link>?
+      </p>      
+    </base-card>
+  </section>
 </template>
